0bc684620da0b81bb63ec5e01e83d3a599d5d347
Kevin Thomas <kevin@mytechnotalent.com>
2023-03-25 19:05:03 -0400

    Update terminal_input.h

--- terminal_input.h
+++ terminal_input.h
@@ -22,16 +22,17 @@
  * SOFTWARE.
 */
 
-/** @file terminal_input.h
+/** 
+ * @file terminal_input.h
  *
- *  @brief Function prototypes for the input driver.
+ * @brief Function prototypes for the input driver.
  * 
- *  This contains the prototypes for the input
- *  driver to have complete control over what will 
- *  be accepted within the input buffer and length
- *  as you can control input to be either strings 
- *  with a length or decimals with a length, floats 
- *  with a length or alphanumerics with a length.
+ * This contains the prototypes for the input
+ * driver to have complete control over what will 
+ * be accepted within the input buffer and length
+ * as you can control input to be either strings 
+ * with a length or decimals with a length, floats 
+ * with a length or alphanumerics with a length.
  * 
  *  @author Kevin Thomas (MyTechnoTalent)
  *  @date   03/25/2023
@@ -55,29 +56,31 @@
 #define DEL             0x7f
 #define RETURN          0xd
 
-/** @brief Function to obtain controlled input from the STDIN.
+/** 
+ * @brief Function to obtain controlled input from the STDIN.
  * 
- *  If the type parameter 'f' is provided it will handle float 
- *  inputs at a given length.  If the type parameter 'd' is 
- *  provided it will handle decimal inputs at a given length.  
- *  If the type parameter 's' is given it will handle string
- *  inputs at a given length.  If the type parameter 'a' is
- *  given it will handle alphanumeric characters at a given
- *  length.
+ * If the type parameter 'f' is provided it will handle float 
+ * inputs at a given length.  If the type parameter 'd' is 
+ * provided it will handle decimal inputs at a given length.  
+ * If the type parameter 's' is given it will handle string
+ * inputs at a given length.  If the type parameter 'a' is
+ * given it will handle alphanumeric characters at a given
+ * length.
  * 
- *  @param type              The type of data looking to capture.
- *  @param usb_char          The individual char to process.
- *  @param usb_string        The built string from proper input.
- *  @param USB_STRING_SIZE   The max size of chars to accept from STDIN.
+ * @param type              The type of data looking to capture.
+ * @param usb_char          The individual char to process.
+ * @param usb_string        The built string from proper input.
+ * @param USB_STRING_SIZE   The max size of chars to accept from STDIN.
  */
 void terminal_input(char type, char *usb_char, char *usb_string, const int *USB_STRING_SIZE);
 
-/** @brief Function to flush the STDIN buffer.
+/**
+ * @brief Function to flush the STDIN buffer.
  * 
- *  This will flush the input buffer string that was
- *  obtained from STDIN. 
+ * This will flush the input buffer string that was
+ * obtained from STDIN. 
  * 
- *  @param usb_string The string from STDIN.
+ * @param usb_string The string from STDIN.
  */
 void terminal_flush_input(char *usb_string);
 
